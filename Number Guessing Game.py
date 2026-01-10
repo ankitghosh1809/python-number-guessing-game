ad5517b0b653d9d78a0114516a8942e2e242f89c
Ankit <ankit.ghosh1648@gmail.com>
2026-01-10 11:34:01 +0530

    Enhance feedback messages in the Number Guessing Game

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -6,12 +6,15 @@ while True:
     if abs(a-b)==10:
         if a>b:
             print("Too High")
+            print("The Correct Number is: ",b)
         else:
             print("Too Low")
+            print("The Correct Number is: ",b)
     elif a==b:
         print("You Guessed The Right Number")
     else:
         print("You Choose A Close Number")
+        print("The Correct Number is: ",b)
     c=input("Do You Want To Play Again:")
     if c.upper()=="NO":
         print("Thank You")
